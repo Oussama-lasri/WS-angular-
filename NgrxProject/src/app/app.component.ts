3433d90eb956903c5d424352593963bf99e0b157
Oussama-lasri <oussama.lasri.2k@gmail.com>
2024-02-26 23:35:53 +0100

    ws-angular

---
+++ NgrxProject/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { AsyncPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, AsyncPipe ,CounterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'NgrxProject';
+}
---
+++ NgrxProject/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    // {
+    //     path: 'counter',
+    //     loadComponent: () =>
+    //       import('./counter/counter.component').then((a) => a.CounterComponent),
+    //   },
+
+];
